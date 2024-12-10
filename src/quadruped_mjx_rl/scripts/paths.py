073c563f9a841e58c62986448d15c28eb0d20d36
Alexei Platzer <ge65tid@tum.de>
2024-12-10 16:02:53 +0100

    bugfix: remove relative paths

--- src/quadruped_mjx_rl/scripts/paths.py
+++ src/quadruped_mjx_rl/scripts/paths.py
@@ -1,4 +1,6 @@
-from src.quadruped_mjx_rl.training_environments import PROJECT_ROOT_PATH
+from etils import epath
+
+PROJECT_ROOT_PATH = epath.Path(__file__).parent.parent
 
 ckpt_path = PROJECT_ROOT_PATH / 'quadrupred_joystick/ckpts'
 ckpt_path.mkdir(parents=True, exist_ok=True)
