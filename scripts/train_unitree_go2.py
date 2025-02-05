a2b75c1fbe82393cbc6bed596c5d16e9a253ff04
Alexei Platzer <ge65tid@tum.de>
2025-02-05 17:57:29 +0100

    remove additional geoms from go2 xml to avoid constant folding with complicated collison geoms

---
+++ scripts/train_unitree_go2.py
@@ -0,0 +1,12 @@
+from quadruped_mjx_rl.training.ppo_simple import train
+
+import paths
+
+if __name__ == '__main__':
+    train(
+        rl_config_path=paths.ppo_simple_config,
+        robot_config_path=paths.unitree_go2_config,
+        init_scene_path=paths.unitree_go2_init_scene,
+        model_save_path=paths.TRAINED_POLICIES_DIRECTORY / "try_simple_ppo_unitree_go2",
+        checkpoints_save_path=paths.ckpt_path,
+    )
