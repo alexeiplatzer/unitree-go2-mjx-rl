9ab594fd97016dc1e2dfb5e85d66d3580e5d0502
Alexei Platzer <ge65tid@tum.de>
2025-02-06 10:54:08 +0100

    default to a complete simple ppo training;

--- scripts/train_unitree_go2.py
+++ scripts/train_unitree_go2.py
@@ -7,6 +7,6 @@ if __name__ == '__main__':
         rl_config_path=paths.ppo_simple_config,
         robot_config_path=paths.unitree_go2_config,
         init_scene_path=paths.unitree_go2_init_scene,
-        model_save_path=paths.TRAINED_POLICIES_DIRECTORY / "try_simple_ppo_unitree_go2",
+        model_save_path=paths.TRAINED_POLICIES_DIRECTORY / "simple_ppo_unitree_go2",
         checkpoints_save_path=paths.ckpt_path,
     )
