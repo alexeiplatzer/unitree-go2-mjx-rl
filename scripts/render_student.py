4e7eb6f3ac1bac3a0e34432c313b677f94df5503
Alexei Platzer <ge65tid@tum.de>
2025-03-09 00:40:58 +0100

    bugfix: bracket in ppo_simple.yaml

--- scripts/render_student.py
+++ scripts/render_student.py
@@ -6,13 +6,6 @@ from quadruped_mjx_rl import render
 
 
 if __name__ == "__main__":
-    teacher_params = model.load_params(paths.TRAINED_POLICIES_DIRECTORY / "ppo_teacher_policy")
-    student_params = model.load_params(paths.TRAINED_POLICIES_DIRECTORY / "ppo_student_policy")
-    model.save_params(
-        paths.TRAINED_POLICIES_DIRECTORY / "ppo_teacher_student_policy",
-        (teacher_params, student_params),
-    )
-
     render(
         paths.ppo_enhanced_config,
         paths.unitree_go2_config,
@@ -20,7 +13,7 @@ if __name__ == "__main__":
         init_scene_path=paths.unitree_go2_init_scene,
         trained_model_path=paths.TRAINED_POLICIES_DIRECTORY / "ppo_teacher_student_policy",
         animation_save_path={
-            "teacher": paths.ANIMATIONS_DIRECTORY / "unitree_go2_teacher_try1.gif",
-            "student": paths.ANIMATIONS_DIRECTORY / "unitree_go2_student_try1.gif",
+            "teacher": paths.ANIMATIONS_DIRECTORY / "unitree_go2_teacher_try2.gif",
+            "student": paths.ANIMATIONS_DIRECTORY / "unitree_go2_student_try2.gif",
         },
     )
