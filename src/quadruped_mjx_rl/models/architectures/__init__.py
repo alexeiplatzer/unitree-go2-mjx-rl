ab51481bd4974c5c0632d55695274fafdb9553d3
Alexei Platzer <ge65tid@tum.de>
2025-05-20 18:03:00 +0200

    bugfix: relative imports error

--- src/quadruped_mjx_rl/models/architectures/__init__.py
+++ src/quadruped_mjx_rl/models/architectures/__init__.py
@@ -1,2 +1,2 @@
-from raw_actor_critic import ActorCriticNetworks
-from guided_actor_critic import TeacherNetworks, StudentNetworks
+from quadruped_mjx_rl.models.architectures.raw_actor_critic import ActorCriticNetworks
+from quadruped_mjx_rl.models.architectures.guided_actor_critic import TeacherNetworks, StudentNetworks
