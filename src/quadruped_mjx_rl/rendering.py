3da88bea5a64fee8da8297e61fa75b0429acdc4a
Alexei Platzer <ge65tid@tum.de>
2025-06-02 21:48:13 +0200

    remove explicit lights from rendering scene

--- src/quadruped_mjx_rl/rendering.py
+++ src/quadruped_mjx_rl/rendering.py
@@ -5,9 +5,10 @@ from functools import partial
 import jax
 import jax.numpy as jp
 import mediapy as media
+import mujoco
 from brax import envs
 from brax.envs.base import PipelineEnv
-from etils.epath import PathLike
+from etils.epath import PathLike, Path
 
 from quadruped_mjx_rl.models import load_inference_fn
 from quadruped_mjx_rl.models import ModelConfig
@@ -86,6 +87,28 @@ def render(
             inference_fn=jax.jit(ppo_inference_fn),
             save_path=animation_save_path,
         )
+
+
+def render_scene(
+    scene_path: PathLike,
+    initial_keyframe: str,
+    camera: str | None = None,
+    save_path: PathLike | None = None,
+):
+    """Renders the initial scene from a given xml file"""
+    xml_path = Path(scene_path).as_posix()
+    mj_model = mujoco.MjModel.from_xml_path(xml_path)
+    renderer = mujoco.Renderer(mj_model)
+    init_q = mj_model.keyframe(initial_keyframe).qpos
+    mj_data = mujoco.MjData(mj_model)
+    mj_data.qpos = init_q
+    mujoco.mj_forward(mj_model, mj_data)
+    renderer.update_scene(mj_data, camera=camera)
+    image = renderer.render()
+    if save_path is not None:
+        media.write_image(save_path, image)
+    else:
+        media.show_image(image)
 
 
 def render_rollout(
